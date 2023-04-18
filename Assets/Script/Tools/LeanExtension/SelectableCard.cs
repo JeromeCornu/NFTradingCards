d3315d5852dac0d9227d2ae74fc0e454b6a86be6
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-04-18 11:55:17 +0200

    Added scripts to communicate with Lean Drag correctly

---
+++ Assets/Script/Tools/LeanExtension/SelectableCard.cs
@@ -0,0 +1,63 @@
+using Lean.Common;
+using Lean.Touch;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectableCard : LeanSelectableBehaviour
+{
+    [SerializeField]
+    private bool _isSelectable = true;
+    [SerializeField]
+    private LeanDragTranslate _drag;
+    [SerializeField]
+    private LayerMask _layerMask;
+    Transform _origin;
+
+    public bool IsSelectable
+    {
+        get => _isSelectable; set
+        {
+            _isSelectable = value;
+            _drag.enabled = _isSelectable;
+        }
+    }
+
+    protected override void OnSelected(LeanSelect select)
+    {
+        base.OnSelected(select);
+        _origin = transform.parent;
+        transform.parent = null;
+    }
+    protected override void OnDeselected(LeanSelect select)
+    {
+        base.OnDeselected(select);
+        if (Physics.BoxCast(transform.position, .33f * transform.lossyScale, -transform.forward, out RaycastHit info, Quaternion.identity, 2f, _layerMask.value)
+            && info.transform.gameObject.CompareTag(gameObject.tag))
+        {
+            transform.parent = info.transform.parent.GetComponentInChildren<Layout>().transform;
+        }
+        else
+            transform.parent = _origin;
+    }
+    /*[SerializeField]
+    LeanSelectable[] _selectablesToForward;
+    public void ForwardSelect(LeanSelect l)
+    {
+        Forward(l, true);
+    }
+    public void ForwardDeselect(LeanSelect l)
+    {
+        Forward(l, false);
+    }
+
+    private void Forward(LeanSelect origin, bool state)
+    {
+        if (state)
+            foreach (var s in _selectablesToForward)
+                origin.Select(s);
+        else
+            foreach (var s in _selectablesToForward)
+                origin.Deselect(s);
+    }*/
+}
