100afc48d9c67093f6a766968edd99fe170b4659
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-04-18 15:09:01 +0200

    Selectable now depends on the animator

--- Assets/Script/Tools/LeanExtension/SelectableCard.cs
+++ Assets/Script/Tools/LeanExtension/SelectableCard.cs
@@ -1,5 +1,6 @@
 using Lean.Common;
 using Lean.Touch;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +14,11 @@ public class SelectableCard : LeanSelectableBehaviour
     [SerializeField]
     private LayerMask _layerMask;
     Transform _origin;
+    private CardAnimator _animator;
+    [HideInInspector]
+    public CardData cardData;
+
+    public CardAnimator Animator { get => _animator; set => _animator = value; }
 
     public bool IsSelectable
     {
@@ -27,42 +33,67 @@ public class SelectableCard : LeanSelectableBehaviour
     {
         base.OnSelected(select);
         _origin = transform.parent;
+        _animator.AdjustDepth(true);
         transform.parent = null;
     }
     protected override void OnDeselected(LeanSelect select)
     {
         base.OnDeselected(select);
-        if ((Physics.BoxCast(transform.position, .33f * transform.lossyScale, -transform.forward, out RaycastHit info, Quaternion.identity, 2f, _layerMask.value)
-                ||
-                Physics.BoxCast(transform.position, .33f * transform.lossyScale, transform.forward, out info, Quaternion.identity, 2f, _layerMask.value)
-            )
-            && info.transform.gameObject.CompareTag(gameObject.tag)
-            && info.transform.parent.TryGetComponent<CardZone>(out CardZone zone)
-            )
-        {
-            zone.AddCard(this);
-        }
-        else
-            transform.parent = _origin;
-    }
-    /*[SerializeField]
-    LeanSelectable[] _selectablesToForward;
-    public void ForwardSelect(LeanSelect l)
-    {
-        Forward(l, true);
-    }
-    public void ForwardDeselect(LeanSelect l)
-    {
-        Forward(l, false);
+        _animator.AdjustDepth(false);
+        Release();
     }
 
-    private void Forward(LeanSelect origin, bool state)
+    private void Release()
     {
-        if (state)
-            foreach (var s in _selectablesToForward)
-                origin.Select(s);
+        if (ValidateCardCost(cardData.Cost))
+        {
+            if (CheckAreaToLockIn(out CardZone zone))
+            {
+                zone.AddCard(this);
+                return;
+            }
+        }
         else
-            foreach (var s in _selectablesToForward)
-                origin.Deselect(s);
-    }*/
+            _animator.CostTooHigh(cardData.Cost);
+        _animator.Reparent(_origin);
+    }
+
+    private bool ValidateCardCost(int cost)
+    {
+        return UnityEngine.Random.Range(0f, 1f) > .25f;
+    }
+
+    private bool CheckAreaToLockIn(out CardZone zone)
+    {
+        zone = null;
+        return CastBoxUpAndDown(out RaycastHit info)
+               && info.transform.gameObject.CompareTag(gameObject.tag)
+               && info.transform.parent.TryGetComponent<CardZone>(out zone);
+    }
+
+    private bool CastBoxUpAndDown(out RaycastHit info)
+    {
+        return Physics.BoxCast(transform.position, .33f * transform.lossyScale, -transform.forward, out info, Quaternion.identity, 2f, _layerMask.value)
+               || Physics.BoxCast(transform.position, .33f * transform.lossyScale, transform.forward, out info, Quaternion.identity, 2f, _layerMask.value);
+    }
+    /*[SerializeField]
+LeanSelectable[] _selectablesToForward;
+public void ForwardSelect(LeanSelect l)
+{
+Forward(l, true);
+}
+public void ForwardDeselect(LeanSelect l)
+{
+Forward(l, false);
+}
+
+private void Forward(LeanSelect origin, bool state)
+{
+if (state)
+foreach (var s in _selectablesToForward)
+ origin.Select(s);
+else
+foreach (var s in _selectablesToForward)
+ origin.Deselect(s);
+}*/
 }
