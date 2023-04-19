9e9014556be9570848b6b373cdd51291864eee03
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-04-19 14:49:23 +0200

    IsTurn Node

---
+++ Assets/BehaviourTreeNodes/CheckIsTurn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UniBT;
+using UnityEngine;
+
+public class CheckIsTurn : Action
+{
+    private GameSystem _game;
+    public override void Awake()
+    {
+        base.Awake();
+    }
+    protected override Status OnUpdate()
+    {
+        _game ??= BT_Blackboard.GameObjects?["Game"].GetComponent<GameSystem>();
+        if (_game == null || _game.TurnManager.IsPlayerTurn)
+        {
+            return Status.Failure;
+        }
+        return Status.Success;
+    }
+
+}
