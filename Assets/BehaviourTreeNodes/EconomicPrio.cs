a79c1262448a9ca1501bd301c56c5df73c2a8dc1
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-04-19 17:46:09 +0200

    Economic prio inmplementation

---
+++ Assets/BehaviourTreeNodes/EconomicPrio.cs
@@ -0,0 +1,22 @@
+using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EconomicPrio : PillardCalculation
+{
+    [SerializeField,Label("MaxNbOfPlayableCardPerTurnToConsiderTooMuchMoney")]
+    private int _nbOfCard = 6;
+
+    protected override string outputKey => "EconomicP";
+
+    protected override float DeterminOutputPriority()
+    {
+        //We base ourselve on the AI dekc always, not considering which we are targetting, considering we know our deck but not the opponent ones, we still consider it will have same average
+        var avgCost = _game.getDeckBehaviour(1).CalculateAverageCost();
+        var currentMoney = player.Money;
+        int playableCard = (int)(currentMoney / avgCost);
+        return (Mathf.Clamp01(playableCard / (float)_nbOfCard));
+    }
+}
