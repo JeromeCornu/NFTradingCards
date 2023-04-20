b7d972f8d9048d00267c0a3747f6463098162e54
MrNyvle <elvynwakeford@gmail.com>
2023-04-20 04:25:01 +0200

    AI Works, not tested in real situation
    Co-Authored-By: superzero4 <78915984+superzero4@users.noreply.github.com>

--- Assets/BehaviourTreeNodes/DefOrAggro.cs
+++ Assets/BehaviourTreeNodes/DefOrAggro.cs
@@ -1,20 +1,19 @@
 using System.Collections.Generic;
+using UnityEngine;
 using UniBT;
+using System.Data.SqlTypes;
 
 namespace BehaviourTreeNodes
 {
     public class DefOrAggro : Action
     {
+        [SerializeField] private int MoneyThresholdForDefence;
+        [SerializeField] private int TemperatureThresholdForDefence;
+        [SerializeField] private int PplSatThresholdForDefence;
+
 
         private static GameSystem _gameSystem;
-        private int _playerCardNumber;
-        private int _aiCardNumber;
-        //Order in list : Money, Temp, Satisfacion;
-        private List<int> _playerStats = new();
-        private List<int> _aiStats = new();
-        public const string StatKey = "StatList";
-
-
+      
 
         protected override Status OnUpdate()
         {
@@ -22,38 +21,26 @@ namespace BehaviourTreeNodes
             _gameSystem = BT_Blackboard.GameObjects["Game"].GetComponent<GameSystem>();
             if (_gameSystem == null) return Status.Failure;
 
+            int Money = 0;
+            int Temp = 0;
+            int PplSat = 0;
 
-            _playerCardNumber = _gameSystem.getDeckBehaviour(0).MHand.Count;
-            _aiCardNumber = _gameSystem.getDeckBehaviour(1).MHand.Count;
+            GameSystem.Player player = _gameSystem[1];
 
-            _playerStats.Add(_gameSystem[0].Money);
-            _playerStats.Add(_gameSystem[0].Temperature);
-            _playerStats.Add(_gameSystem[0].PeopleSatistfaction);
-            _aiStats.Add(_gameSystem[1].Money);
-            _aiStats.Add(_gameSystem[1].Temperature);
-            _aiStats.Add(_gameSystem[1].PeopleSatistfaction);
-
-            float averagePlayerStat;
-            float averageAiStat;
-
-            float avrgMoney = (float)(_playerStats[0] - _gameSystem.MStartMoney) / _gameSystem.MStartMoney;
-            float avrgTemp = -(float)(_playerStats[1] - _gameSystem.MStartTemp) / _gameSystem.MStartTemp;
-            float avrgPplSat = (float)(_playerStats[2] - _gameSystem.MStartSatisfaction) / _gameSystem.MStartSatisfaction;
-
-            //(va-vi)/vi
-
-            averagePlayerStat = (avrgMoney + avrgTemp + avrgPplSat) / _playerStats.Count;
-
-            avrgMoney = (float)(_aiStats[0] - _gameSystem.MStartMoney) / _gameSystem.MStartMoney;
-            avrgTemp = -(float)(_aiStats[1] - _gameSystem.MStartTemp) / _gameSystem.MStartTemp;
-            avrgPplSat = (float)(_aiStats[2] - _gameSystem.MStartSatisfaction) / _gameSystem.MStartSatisfaction;
-
-            averageAiStat = (avrgMoney + avrgTemp + avrgPplSat) / _aiStats.Count;
-
-
-            if (_aiCardNumber > _playerCardNumber && averageAiStat > averagePlayerStat)
+            foreach (Card card in player.CardOnBoard)
             {
-                BT_Blackboard.Bools["bAggro"] = true;
+                Money += card.CardData[CardData.Pillar.Economic].Val;
+                Temp -= card.CardData[CardData.Pillar.Ecologic].Val;
+                PplSat += card.CardData[CardData.Pillar.Social].Val;
+            }
+
+            if(Money > MoneyThresholdForDefence && Temp < TemperatureThresholdForDefence && PplSat > PplSatThresholdForDefence) 
+            {
+                BT_Blackboard.Bools[DecideCard.aggroKey] = true;
+            }
+            else
+            {
+                BT_Blackboard.Bools[DecideCard.aggroKey] = false;
             }
 
             //Force ; BT_Blackboard.Bools["bAggro"] = true;
