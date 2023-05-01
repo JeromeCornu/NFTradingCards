5f9608a2d45d94390c7b3a4342207981281ebed7
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-05-01 19:52:13 +0200

    Can also scale on y, if we need and provided measures for y

--- Assets/Script/Tools/BackgroundAligner.cs
+++ Assets/Script/Tools/BackgroundAligner.cs
@@ -6,19 +6,34 @@ public class BackgroundAligner : MonoBehaviour
     {
         AdjustDepth();
     }
-    void AdjustDepth(Vector2 _originalScreenSize, Vector3 _originalDepth, Vector2 _adjustedScreenSize, Vector3 _adjustedDepth)
+    void AdjustDepth(Vector2 _originalScreenSize, Vector2 _originalScale, Vector2 _adjustedScreenSize, Vector2 _adjustedScale)
     {
-        float currentAspectRatio = (float)Screen.width / Screen.height;
+        var x = LerpRelative(_originalScreenSize.x, _adjustedScreenSize.x, _originalScale.x, _adjustedScale.x, Screen.width);
+        //var y = LerpRelative(_originalScreenSize.y, _adjustedScreenSize.y, _originalScale.y, _originalScale.y, Screen.width);
+        transform.localScale = new Vector3(x, 1,1);
+    }
+    /// <summary>
+    /// Inverses lerp tToRelativize beetween v1 and v2, and lerp with this obtained value beetween a and b
+    /// Given, two points and their corresponding values, we can calculate a new value on the line drawn beetween the first two couples
+    /// </summary>
+    /// <param name="v1"></param>
+    /// <param name="v2"></param>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <param name="tToRelativize"></param>
+    /// <returns></returns>
+    private float LerpRelative(float v1, float v2, float a, float b, float tToRelativize)
+    {
+        tToRelativize = (tToRelativize - v1) / (v2 - v1);
+        //If is nan, meands v1=v2, we take the halfway point, but it especially means we provided same values for v1 and v2 which doesn't mean a thing
+        if (float.IsNaN(tToRelativize))
+            tToRelativize = .5f;
+        return tToRelativize * b + (1 - tToRelativize) * a;
 
-        float originalAspectRatio = _originalScreenSize.x / _originalScreenSize.y;
-        float adjustedAspectRatio = _adjustedScreenSize.x / _adjustedScreenSize.y;
-        float aspectRatioDifference = (currentAspectRatio - originalAspectRatio) / (adjustedAspectRatio - originalAspectRatio);
-        var adjustedDepth = aspectRatioDifference * _adjustedDepth + (1 - aspectRatioDifference) * _originalDepth;
-        transform.localScale = adjustedDepth;
     }
     void AdjustDepth()
     {
         //Reference values, found iteratively, to always scale the background correctly, change here if the background size tend to change
-        AdjustDepth(new Vector2(1920, 1080), Vector3.one, new Vector2(2400, 1080), new Vector3(1.17f,1,1));
+        AdjustDepth(new Vector2(1920, 1080), Vector3.one, new Vector2(2400, 1080), new Vector3(1.17f, 1, 1));
     }
 }
