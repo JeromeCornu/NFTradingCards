52e40d8b13b00d577957bf48534fb5b2c4d7ae5b
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-05-01 19:37:43 +0200

    Adjusted aligner to scale instead of moving was overlapping the game)

--- Assets/Script/Tools/BackgroundAligner.cs
+++ Assets/Script/Tools/BackgroundAligner.cs
@@ -6,19 +6,19 @@ public class BackgroundAligner : MonoBehaviour
     {
         AdjustDepth();
     }
-    void AdjustDepth(Vector2 _originalScreenSize, float _originalDepth, Vector2 _adjustedScreenSize, float _adjustedDepth)
+    void AdjustDepth(Vector2 _originalScreenSize, Vector3 _originalDepth, Vector2 _adjustedScreenSize, Vector3 _adjustedDepth)
     {
         float currentAspectRatio = (float)Screen.width / Screen.height;
 
         float originalAspectRatio = _originalScreenSize.x / _originalScreenSize.y;
         float adjustedAspectRatio = _adjustedScreenSize.x / _adjustedScreenSize.y;
         float aspectRatioDifference = (currentAspectRatio - originalAspectRatio) / (adjustedAspectRatio - originalAspectRatio);
-        float adjustedDepth = aspectRatioDifference * _adjustedDepth + (1 - aspectRatioDifference) * _originalDepth;
-        transform.position = new Vector3(transform.position.x, transform.position.y, adjustedDepth);
+        var adjustedDepth = aspectRatioDifference * _adjustedDepth + (1 - aspectRatioDifference) * _originalDepth;
+        transform.localScale = adjustedDepth;
     }
     void AdjustDepth()
     {
         //Reference values, found iteratively, to always scale the background correctly, change here if the background size tend to change
-        AdjustDepth(new Vector2(1920, 1080), 0f, new Vector2(2400, 1080), -2f);
+        AdjustDepth(new Vector2(1920, 1080), Vector3.one, new Vector2(2400, 1080), new Vector3(1.17f,1,1));
     }
 }
