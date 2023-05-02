941bbba5f891f256cd637501d9588583df2b6a00
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-05-02 21:16:02 +0200

    Now stopping play when quitting from editor

--- Assets/Script/Menu/MainMenu.cs
+++ Assets/Script/Menu/MainMenu.cs
@@ -104,7 +104,10 @@ public class MainMenu : MonoBehaviour
     public void Quit()
     {
         soundManager.PlaySound(buttonSound);
-        print("Quit");
+#if !UNITY_EDITOR
         Application.Quit();
+#else
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
