b48512383f08b4b6be9a46fdd8d48059e1eb0564
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-05-14 22:33:00 +0200

    Draft of implementaing reusable tweens creation at start and calling from external methods

--- Assets/Script/Tools/PositionTweener.cs
+++ Assets/Script/Tools/PositionTweener.cs
@@ -6,24 +6,39 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class PositionTweener : MonoBehaviour
 {
     public TweenParameter[] _availablePredefinedTweens;
+    public List<Tweener> _tweens = new List<Tweener>();
+    //In case we don't override the duration when trying to call a tween
+    private const float DEFAULTTWEENDURATION=1f;
     private void Start()
     {
-        /*var t = DOTween.Sequence().Ap;
-        DOTween.DoSp*/
+        RegisterReplayableTween(DOTweenProShortcuts.DOSpiral((Transform)null, DEFAULTTWEENDURATION, new Vector3(0, 1, 1)));
+        RegisterReplayableTween(ShortcutExtensions.DORotate(transform, new Vector3(0, 360, 0), DEFAULTTWEENDURATION,RotateMode.FastBeyond360));
     }
-
-    public Tween StartTween(Transform target, Vector3 worldDest, TweenParameter parameter)
+    private void RegisterReplayableTween(Tweener tween)
     {
-        return StartTween(target, worldDest, parameter, () => { return; });
+        tween.Pause();
+        tween.target = null;
+        _tweens.Add(tween);
     }
-    public Tween StartTween(Transform target, Vector3 worldDest, TweenParameter parameter, TweenCallback onComplete)
+    public Tween PlayTween(int registeredTween,Transform target, Vector3 worldDest, TweenCallback onComplete = null)
     {
-        return target.DOMove(worldDest, parameter._duration).SetEase(parameter._ease).OnComplete(onComplete);
-        //target.DoSpiral()
+        return StartTween(_tweens[registeredTween], target, worldDest, _availablePredefinedTweens[0], onComplete);
+    }
+    public Tween StartTween(Tweener tween, Transform target, Vector3 worldDest, TweenParameter parameter, TweenCallback onComplete = null)
+    {
+        tween.target = target;
+        tween.ChangeEndValue(worldDest, parameter._duration);
+        tween.SetEase(parameter._ease);
+        if (onComplete != null)
+            tween.OnComplete(onComplete);
+        tween.OnComplete(() => tween.Rewind());
+        tween.Restart();
+        return tween;
     }
 }
 [System.Serializable]
@@ -33,5 +48,4 @@ public struct TweenParameter
     public readonly float _duration;
     [SerializeField]
     public readonly Ease _ease;
-
 }
